11c992c1396f835209042f1c8f2f56b9433dc8aa
Benjamín Serrano Friedlander <benser22@hotmail.com>
2023-06-05 07:31:35 -0300

    autocomplete off

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,7 @@ import '../styles/Search.css'
 export default function SearchBar(props) {
    return (
       <div className="search_style">
-         <input type="search" id="id" name="q" />
+         <input autocomplete="off" type="search" id="id" name="q" />
          <button onClick={props.onSearch} className='search_button'>Agregar</button>
       </div>
    );
