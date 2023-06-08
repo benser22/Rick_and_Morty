1061c807d73bff668f5d1ab67c9b7522ec94520e
Benjamín Serrano Friedlander <benser22@hotmail.com>
2023-06-08 13:43:39 -0300

    log img added to element search

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,8 +1,15 @@
 import "../SearchBar/Search.css";
+import ramtitle from '../../assets/images/titleBar.png'
 
+console.log(ramtitle);
 export default function SearchBar(props) {
   return (
     <div className="search_style">
+       <img
+          className="imageBar"
+          src={ramtitle}
+          alt={"Title Rick and Morty"}
+        />
       <input autoComplete="off" type="search" id="id" name="q" />
       <button onClick={props.onSearch} className="mybutton">
         Buscar
