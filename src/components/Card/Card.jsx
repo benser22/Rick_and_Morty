fb12e2c0db33115db21ec5048885171bafe638b3
Benjamín Serrano Friedlander <benser22@hotmail.com>
2023-06-08 18:56:38 -0300

    modularization  card styles from css to module.css

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,13 @@
 import React, { useState } from "react";
-import "../Card/Card.css";
-import "../Modal/Modal.css";
 import { pictures, gifs } from "../Pictures";
-import styles from "./FootCard.module.css";
+import foot from "./FootCard.module.css";
+import styles from './Card.module.css';
 
 export default function Card(props) {
   const [myroot, setMyroot] = useState(props.image);
+  const [mygif] = useState(props.gifs);
   const [isHovered, setIsHovered] = useState(false);
   const [visibility, setVisibility] = useState(true);
-
-  // console.log("previa: ", props.name, "  actual: ", "next ");
 
   const handleHover = () => {
     setIsHovered(true);
@@ -41,49 +39,52 @@ export default function Card(props) {
 
   return (
     <div
-      className={`card ${props.inFocus ? "visible" : ""} ${
-        props.prevCard ? "prev visible" : ""
-      } ${props.nextCard ? "next visible" : ""} ${
-        visibility ? "" : "invisible"
-      } ${isHovered ? "hovered" : ""}`}
+      className={`${styles.card} ${props.inFocus ? styles.visible : ""} ${
+        props.prevCard ? `${styles.prev} ${styles.visible}` : ""
+      } ${props.nextCard ? `${styles.next} ${styles.visible}` : ""} ${
+        visibility ? "" : styles.invisible
+      } ${isHovered ? styles.hovered : ""}`}
       onClick={clickCard}
     >
-      <button className="mybutton rig" onClick={visibility ? onClose : onOpen}>
-        {" "}
-        {props.id} X{" "}
+      <button
+        className={`${styles.mybutton} ${styles.rig}`}
+        onClick={visibility ? onClose : onOpen}
+      >
+        {props.id} X
       </button>
-      <h2 className="name">
-        <span className="value">{props.name}</span>
+      <h2 className={styles.name}>
+        <span className={styles.value}>{props.name}</span>
       </h2>
-      <h2 className="property">
-        Status: <span className="value">{props.status}</span>
+      <h2 className={styles.property}>
+        Status: <span className={styles.value}>{props.status}</span>
       </h2>
-      <h2 className="property">
-        Species: <span className="value">{props.species}</span>
+      <h2 className={styles.property}>
+        Species: <span className={styles.value}>{props.species}</span>
       </h2>
-      <h2 className="property">
-        Gender: <span className="value">{props.gender}</span>
+      <h2 className={styles.property}>
+        Gender: <span className={styles.value}>{props.gender}</span>
       </h2>
-      <h2 className="property">
-        Origin: <span className="value">{props.origin.name}</span>
+      <h2 className={styles.property}>
+        Origin: <span className={styles.value}>{props.origin.name}</span>
       </h2>
-      <div className={styles.cont}>
-      <span className={`${styles.prev} ${props.inFocus ? "" : styles.empty}`} onClick={props.goToPreviousCard}>
-        {" "}
-      </span>
-      <img
-        className={styles.characters}
-        src={myroot}
-        alt={"Imagen de " + props.name}
-        width="220"
-        height="213"
-        onMouseEnter={handleHover}
-        onMouseLeave={handleLeave}
-      />
-      <span className={`${styles.next} ${props.inFocus ? "" : styles.empty}`} onClick={props.goToNextCard}>
-        {" "}
-      </span>
-
+      <div className={foot.contFoot}>
+        <span
+          className={`${foot.prev} ${props.inFocus ? "" : foot.empty}`}
+          onClick={props.goToPreviousCard}
+        ></span>
+        <img
+          className={`${foot.characters} ${props.inFocus ? "" : foot.static}`}
+          src={props.inFocus ? myroot : props.image}
+          alt={"Imagen de " + props.name}
+          width="220"
+          height="213"
+          onMouseEnter={handleHover}
+          onMouseLeave={handleLeave}
+        />
+        <span
+          className={`${foot.next} ${props.inFocus ? "" : foot.empty}`}
+          onClick={props.goToNextCard}
+        ></span>
       </div>
     </div>
   );
