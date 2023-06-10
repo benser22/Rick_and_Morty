bff56af3d056ee56d51af2bcdc4f74b15769150a
Benjamín Serrano Friedlander <benser22@hotmail.com>
2023-06-10 00:03:54 -0300

    warning fixed

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,46 +1,46 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Card from "../Card/Card";
-import "../Cards/Container.css";
+import styles from "../Cards/Cards.module.css";
 
 export default function Cards(props) {
   const [currentCard, setCurrentCard] = useState(0);
   const [divClass, setDivClass] = useState("container");
 
-  const goToPreviousCard = () => {
+  const goToPreviousCard = useCallback(() => {
     setCurrentCard((prevCurrentCard) =>
       prevCurrentCard === 0 ? props.characters.length - 1 : prevCurrentCard - 1
     );
-  };
+  }, [props.characters.length]);
 
-  const goToNextCard = () => {
+  const goToNextCard = useCallback(() => {
     setCurrentCard((prevCurrentCard) =>
       prevCurrentCard === props.characters.length - 1 ? 0 : prevCurrentCard + 1
     );
-  };
-
-  const handleScroll = (event) => {
-    const direction = event.deltaY > 0 ? "down" : "up";
-
-    if (direction === "down") {
-      goToNextCard();
-    } else {
-      goToPreviousCard();
-    }
-  };
+  }, [props.characters.length]);
 
   useEffect(() => {
+    const handleScroll = (event) => {
+      const direction = event.deltaY > 0 ? "down" : "up";
+
+      if (direction === "down") {
+        goToNextCard();
+      } else {
+        goToPreviousCard();
+      }
+    };
+
     window.addEventListener("wheel", handleScroll);
 
     return () => {
       window.removeEventListener("wheel", handleScroll);
     };
-  }, []);
+  }, [goToNextCard, goToPreviousCard]);
 
   useEffect(() => {
     if (currentCard === 0 || currentCard === props.characters.length - 1) {
-      setDivClass("container inverse");
+      setDivClass(`${styles.container} ${styles.inverse}`);
     } else {
-      setDivClass("container");
+      setDivClass(styles.container);
     }
   }, [currentCard, props.characters.length]);
 
@@ -51,8 +51,14 @@ export default function Cards(props) {
           {...element}
           key={element.id}
           inFocus={index === currentCard}
-          prevCard={index === currentCard - 1 || (currentCard === 0 && index === props.characters.length - 1)}
-          nextCard={index === currentCard + 1 || (currentCard === props.characters.length - 1 && index === 0)}
+          prevCard={
+            index === currentCard - 1 ||
+            (currentCard === 0 && index === props.characters.length - 1)
+          }
+          nextCard={
+            index === currentCard + 1 ||
+            (currentCard === props.characters.length - 1 && index === 0)
+          }
           goToNextCard={goToNextCard}
           goToPreviousCard={goToPreviousCard}
         />
