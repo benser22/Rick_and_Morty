3f643872199368e6bdd5a764ff0c6429f12652c2
Benjamín Serrano Friedlander <benser22@hotmail.com>
2023-06-15 11:26:51 -0300

    nav, about, random: ok

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./About.module.css";
 import profileImage from "../../assets/images/aboutme.webp";
-import { FaHome } from "react-icons/fa";
+import { FaHome, FaLinkedin, FaGithub } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 
 const About = () => {
@@ -9,7 +9,7 @@ const About = () => {
     <div className={styles.aboutContainer}>
       <div className={styles.background} />
       <div className={styles.content}>
-        <h2>About me</h2>
+        <h2 className={styles.me}>About me</h2>
         <div className={styles.profileImageContainer}>
           <img
             src={profileImage}
@@ -17,15 +17,33 @@ const About = () => {
             className={styles.profileImage}
           />
         </div>
-        <p className={styles.description}>
-          Hello! I'm Benjamin, a full stack web developer.
+        <p className={styles.begin}>
+          Hello! I'm Benjamin, a full stack web developer...
         </p>
         <p className={styles.description}>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+          ...Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium
           nostrum sit neque dicta commodi atque facilis vitae nobis molestiae
           reprehenderit esse doloribus, totam asperiores laboriosam fuga quod?
           Delectus, ex neque!
         </p>
+        <div>
+          <a
+            href="https://www.linkedin.com/in/benjam%C3%ADn-serrano-friedlander-30527b247/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.navLink}
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            href="https://github.com/benser22"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.navLink}
+          >
+            <FaGithub />
+          </a>
+        </div>
       </div>
       <NavLink to="/home" className={styles.navLink}>
         <FaHome />
