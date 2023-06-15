c79862b2dc9ab9a4bebbc34f7e2e1623ad2d5586
Benjamín Serrano Friedlander <benser22@hotmail.com>
2023-06-15 20:56:18 -0300

    pass validation ok

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Form.module.css";
 
 const Form = ({ userData, handleChange, login, setFormSubmitted }) => {
+  const [passwordError, setPasswordError] = useState("");
+
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    if (!/\d/.test(userData.password)) {
+      setPasswordError("La contraseña debe contener al menos un número.");
+      return;
+    }
+
+    if (userData.password.length < 6 || userData.password.length > 10) {
+      setPasswordError("La contraseña debe tener una longitud entre 6 y 10 caracteres.");
+      return;
+    }
+
+    setPasswordError("");
     login(userData);
     setFormSubmitted(true);
   };
@@ -13,7 +27,7 @@ const Form = ({ userData, handleChange, login, setFormSubmitted }) => {
       <h2>Sign in</h2>
       <form onSubmit={handleSubmit}>
         <label htmlFor="email">E-mail:</label>
-        <input 
+        <input
           placeholder="example@example.com"
           type="email"
           id="email"
@@ -34,6 +48,7 @@ const Form = ({ userData, handleChange, login, setFormSubmitted }) => {
           onChange={handleChange}
           autoComplete="new-password"
         />
+        {passwordError && <p className={styles.error}>{passwordError}</p>}
 
         <button type="submit">Submit</button>
       </form>
