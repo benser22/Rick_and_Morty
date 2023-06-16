e2eeb250acc20487f561825a70b6e23fb124bc9e
Benjamín Serrano Friedlander <benser22@hotmail.com>
2023-06-16 12:26:41 -0300

    react root fixed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 );
