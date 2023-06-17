db2aa96a4fefc635252b6737d92f387599d9e7b1
Benjamín Serrano Friedlander <benser22@hotmail.com>
2023-06-17 18:02:16 -0300

    1.03

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -18,7 +18,7 @@ export default function SearchBar(props) {
       setSearchValue(""); // Limpiar el campo de entrada
       redirectToHome();
     } else {
-      window.alert("El Id debe ser un número entre 0 y 826");
+      window.alert("El Id debe ser un número entre 1 y 826");
     }
   };
   
