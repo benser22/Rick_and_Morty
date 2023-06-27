3e787d90b5ddf2ec15e4a7b9b913320bae021caf
Benjamín Serrano Friedlander <benser22@hotmail.com>
2023-06-27 16:50:00 -0300

    Using redux with class component(map, connect...) In comments: with functional component

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,4 +1,62 @@
 import React from "react";
+import { connect } from "react-redux";
+import Card from "../Card/Card";
+import styles from "../Cards/Cards.module.css";
+import { addToFavorites, removeFromFavorites } from "../../redux/actions/favoritesActions";
+
+class Cards extends React.Component {
+  handleAddToFavorites = (character) => {
+    this.props.addToFavorites(character);
+  };
+
+  handleRemoveFromFavorites = (characterId) => {
+    this.props.removeFromFavorites(characterId);
+  };
+
+  render() {
+    const { onClose, characters, favorites } = this.props;
+
+    return (
+      <div className={styles.container}>
+        {characters.map((element, index) => {
+          const isFavorite = favorites.includes(element.id); 
+          return (
+            <Card
+              element={element}
+              onClose={onClose}
+              key={index}
+              inFocus={index === element.id}
+              isFavorite={isFavorite} 
+              AddToFavorites={this.handleAddToFavorites} 
+              RemoveFromFavorites={this.handleRemoveFromFavorites} 
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    favorites: state.favorites.favorites
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addToFavorites: (character) => dispatch(addToFavorites(character)),
+    removeFromFavorites: (characterId) => dispatch(removeFromFavorites(characterId))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cards);
+
+
+/*
+* CON HOOKS!!! 
+
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Card from "../Card/Card";
 import styles from "../Cards/Cards.module.css";
@@ -37,3 +95,6 @@ const Cards = ({ onClose, characters }) => {
 };
 
 export default Cards;
+
+
+*/
