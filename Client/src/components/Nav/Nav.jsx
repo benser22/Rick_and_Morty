40f81a41f5c6db6cc17c549b51b1d5e85d1545a1
Benjamín Serrano Friedlander <benser22@hotmail.com>
2023-07-17 03:04:38 -0300

    User.Data.Email to Guest

--- Client/src/components/Nav/Nav.jsx
+++ Client/src/components/Nav/Nav.jsx
@@ -13,36 +13,24 @@ import ramtitle from "../../assets/images/title.webp";
 // Estilos
 import styles from "./Nav.module.css";
 
-// Funciones auxiliares de almacenamiento local
-import {
-  saveDataToLocalStorage, // Guardar datos en almacenamiento local
-  getDataFromLocalStorage, // Obtener datos desde almacenamiento local
-} from "../../localStorageUtils";
-
 // Librería de Modal
 import Modal from "react-modal";
-Modal.setAppElement("#root"); /* Especifica el elemento de la aplicación (App element) al utilizar la biblioteca react-modal. Esta línea es necesaria para garantizar el correcto funcionamiento de react-modal en términos de accesibilidad. */
+Modal.setAppElement(
+  "#root"
+); /* Especifica el elemento de la aplicación (App element) al utilizar la biblioteca react-modal. Esta línea es necesaria para garantizar el correcto funcionamiento de react-modal en términos de accesibilidad. */
 
 function Nav(props) {
   const [isHovered, setIsHovered] = useState(false); // Estado para controlar si el mouse está sobre el componente.Lo necesito para mostrar o el mail del usuario o Logout
   const [email, setEmail] = useState(""); // Estado para almacenar el correo electrónico del usuario
   const [logout, setLogout] = useState(false); // Estado para controlar si se ha realizado el cierre de sesión
   const [showModal, setShowModal] = useState(false); // Estado para controlar la visibilidad del modal
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
   const location = useLocation(); // Ubicación actual de la ruta
 
   useEffect(() => {
-    // Obtengo el correo electrónico almacenado para mostrarlo en mi nav
-    const storedEmail = getDataFromLocalStorage("email");
-    if (storedEmail) {
-      // Si hay un correo electrónico almacenado en el almacenamiento local, lo utiliza
-      setEmail(storedEmail); // Actualizo el estado de 'email' con el correo electrónico almacenado
-    } else {
-      // De lo contrario, utiliza el correo electrónico proporcionado en las propiedades
-      setEmail(props.userData.email); // Actualizo el estado de 'email' con el correo electrónico de las propiedades
-      saveDataToLocalStorage("email", props.userData.email); // Guarda el correo electrónico en el almacenamiento local
-    }
-  }, [props.userData.email]);
+    setEmail(props.userData.email);
+    // eslint-disable-next-line
+  }, []);
 
   const handleMouseEnter = () => {
     setIsHovered(true); // Actualizo el estado de 'isHovered' para indicar que el mouse está sobre el componente
@@ -110,10 +98,30 @@ function Nav(props) {
         overlayClassName={styles.modalOverlay}
       >
         <h2 className={styles.myh2}>Confirm Logout</h2>
-        <hr style={{ width: "100%", boxShadow: "0px 5px 10px 0px rgba(0, 0, 0, 0.5)" }}></hr>
-        <p style={{ fontWeight: "bold", fontFamily: "sans-serif", fontSize:"14px" }}>Are you sure you want to log out?</p>
-        <button style={{ fontSize:"14px" }} onClick={handleConfirmLogout}>Logout</button>
-        <button style={{ fontSize:"14px" }} onClick={() => setShowModal(false)}>Cancel</button>
+        <hr
+          style={{
+            width: "100%",
+            boxShadow: "0px 5px 10px 0px rgba(0, 0, 0, 0.5)",
+          }}
+        ></hr>
+        <p
+          style={{
+            fontWeight: "bold",
+            fontFamily: "sans-serif",
+            fontSize: "14px",
+          }}
+        >
+          Are you sure you want to log out?
+        </p>
+        <button style={{ fontSize: "14px" }} onClick={handleConfirmLogout}>
+          Logout
+        </button>
+        <button
+          style={{ fontSize: "14px" }}
+          onClick={() => setShowModal(false)}
+        >
+          Cancel
+        </button>
       </Modal>
     </div>
   );
