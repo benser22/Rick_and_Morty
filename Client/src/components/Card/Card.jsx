5f89ba7aa3ef41549e69f2f9e4c30cc3c7fd1ca5
Benjamín Serrano Friedlander <benser22@hotmail.com>
2023-07-21 10:38:23 -0300

    Test attached to the Card component

--- Client/src/components/Card/Card.jsx
+++ Client/src/components/Card/Card.jsx
@@ -3,30 +3,39 @@ import styles from "./Card.module.css";
 import { Link } from "react-router-dom";
 import { FaHeart } from "react-icons/fa";
 
-export default function Card({element, onClose, isFavorite, AddToFavorites, RemoveFromFavorites, isArrayFavorites,}) {
-  
+export default function Card({
+  element,
+  onClose,
+  isFavorite,
+  AddToFavorites,
+  RemoveFromFavorites,
+  isArrayFavorites,
+}) {
   // Manipula lista de favoritos con las funciones de redux que recibí por parametro
-  const handleToggleFavorite = () => { 
+  const handleToggleFavorite = () => {
     if (isFavorite) {
       RemoveFromFavorites(element.id); // Si ya es favorito, se remueve de la lista de favoritos
     } else {
       AddToFavorites(element); // Si no es favorito, se agrega a la lista de favoritos
     }
   };
-  
+
   return (
-    <div className={styles.myDiv} data-testid="favorite-card">
+    <div className={styles.myDiv} data-testid="card">
       <div className={styles.buttonContainer}>
         {/* Botón de favorito */}
         {!isArrayFavorites && (
           <FaHeart
+            data-testid="heart-icon"
             className={`${styles.heartIcon} ${
               isFavorite ? styles.favorite : ""
             }`}
             onClick={handleToggleFavorite}
           />
         )}
-        <button className={styles.myButton} onClick={() => {
+        <button
+          className={styles.myButton}
+          onClick={() => {
             onClose(element.id); // Cierra el componente al hacer clic en el botón de cierre
           }}
         >
