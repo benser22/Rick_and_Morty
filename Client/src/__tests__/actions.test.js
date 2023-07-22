2fa4bec6ea40a68dab784c38ea8075217afc7dca
Benjamín Serrano Friedlander <benser22@hotmail.com>
2023-07-22 11:16:48 -0300

    Test for "actions" finished

--- Client/src/__tests__/actions.test.js
+++ Client/src/__tests__/actions.test.js
@@ -20,7 +20,7 @@ const mockStore = configureMockStore(middlewares);
 
 jest.mock("axios");
 
-describe("Actions", () => {
+describe("Test Redux: Actions", () => {
   it("should create ADD_FAV action", async () => {
     const characterData = { id: 1, name: "Morty Smith" };
     axios.post.mockResolvedValueOnce({ data: characterData });
@@ -33,5 +33,51 @@ describe("Actions", () => {
     expect(store.getActions()).toEqual(expectedActions);
   });
 
-  // Add more test cases for other actions in actions.js...
+  it("should create REMOVE_FAV action", async () => {
+    const characterIdToRemove = 1;
+    const removedCharacterData = {
+      id: characterIdToRemove,
+      name: "Rick Sanchez",
+    };
+    axios.delete.mockResolvedValueOnce({ data: removedCharacterData });
+
+    const expectedActions = [
+      { type: REMOVE_FAV, payload: removedCharacterData },
+    ];
+    const store = mockStore({});
+
+    await store.dispatch(removeFav(characterIdToRemove));
+
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+
+  it("should create REMOVE_ALL_FAVORITES action", async () => {
+    axios.delete.mockResolvedValueOnce();
+
+    const expectedActions = [{ type: REMOVE_ALL_FAVORITES }];
+    const store = mockStore({});
+
+    await store.dispatch(removeAllFavorites());
+
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+
+  it("should create ORDER action", () => {
+    const order = "D"; // Or any other order you want to test
+    const expectedAction = { type: ORDER, payload: order };
+
+    const action = orderCards(order);
+
+    expect(action).toEqual(expectedAction);
+  });
+
+  it("should create FILTER action", () => {
+    const filter = "Male"; // Or any other filter you want to test
+    const expectedAction = { type: FILTER, payload: filter };
+
+    const action = filterCards(filter);
+
+    expect(action).toEqual(expectedAction);
+  });
+
 });
