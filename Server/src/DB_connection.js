fd07ac9012e842f9bf47f7f5260c0b81ef31e1ae
Benjamín Serrano Friedlander <benser22@hotmail.com>
2023-08-06 14:15:03 -0300

    Deploying App

--- Server/src/DB_connection.js
+++ Server/src/DB_connection.js
@@ -1,24 +1,26 @@
-require('dotenv').config();
-const { Sequelize } = require('sequelize');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 const FavoriteModel = require("./models/Favorite");
 const UserModel = require("./models/User");
 
-const sequelize = new Sequelize(
-   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`,
-   { logging: false, native: false }
-);
+// const sequelize = new Sequelize(
+//    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`,
+//    { logging: false, native: false }
+// );
+
+const sequelize = new Sequelize(DB_DEPLOY, { logging: false, native: false });
 
 FavoriteModel(sequelize);
 UserModel(sequelize);
 
 const { User, Favorite } = sequelize.models;
 
-User.belongsToMany(Favorite, { through: 'UserFavorite' });
-Favorite.belongsToMany(User, { through: 'UserFavorite' });
+User.belongsToMany(Favorite, { through: "UserFavorite" });
+Favorite.belongsToMany(User, { through: "UserFavorite" });
 
 module.exports = {
-   User,
-   Favorite,
-   conn: sequelize,
+  User,
+  Favorite,
+  conn: sequelize,
 };
